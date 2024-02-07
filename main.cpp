5549846adc9131563db6ac91f52a0c9391a4ab23
BrandenMitchell <71188053+BrandenMitchell@users.noreply.github.com>
2024-02-07 17:29:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,10 +30,10 @@ int main() {
          cout << "Couldnt open file" << endl;
      }
 
-     //getInt(80,nums,count); // gave 80, should return 79
-     //modify(5,999,nums,count); // very obvious number '999' passed, at index of 5 should be 999
-     //addInt(420,nums,count); //very obvious number passed
-     //addInt(699,nums,count); //very obvious number passed. add int done twice to show is dynamic
+     getInt(80,nums,count); // gave 80, should return 79
+     modify(5,999,nums,count); // very obvious number '999' passed, at index of 5 should be 999
+     addInt(420,nums,count); //very obvious number passed
+     addInt(699,nums,count); //very obvious number passed. add int done twice to show is dynamic
      replace_remove(5,nums,count);
 
 
